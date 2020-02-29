cf21a6a788a9d95ec13598dd39bc12b3ab1826cc
kolomu <kevin-olomu@web.de>
2020-02-29 08:19:17 +0100

    Added Spring Configuration for ChuckJokes Dependency for constructor DI. (@Configuration, @Bean)

---
+++ src/main/java/com/kolomu/JokesApp/config/ChuckConfiguration.java
@@ -0,0 +1,21 @@
+package com.kolomu.JokesApp.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * This is in same package as JokesApplication therefore it will be picked up by spring as component.
+ * Important to tell spring that the configuration class is returning a bean!
+ * Refactored the implementation to the configuration which gets injected via DI.
+ *
+ * Good use case when dealing with external JARs. (where you don't control the source code)
+ * */
+@Configuration
+public class ChuckConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes() {
+        return new ChuckNorrisQuotes();
+    }
+}
--- src/main/java/com/kolomu/JokesApp/services/JokeServiceImpl.java
+++ src/main/java/com/kolomu/JokesApp/services/JokeServiceImpl.java
@@ -8,9 +8,17 @@ public class JokeServiceImpl implements JokeService {
     // final so it can't be changed and constructor also don't need anything about it.
     private final ChuckNorrisQuotes chuckNorrisQuotes;
 
+    /* Refactoring because we don't want to instantiate with new
     // this class gets created once by spring and we will reuse that. (Not recreated for each request!)
     public JokeServiceImpl() {
         chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+     */
+
+    // Constructor based injected -> now we have to provide chuckNorrisQuote Bean in the context!
+    // see config -> ChuckConfiguration.java
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
     }
 
     @Override
